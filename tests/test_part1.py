7df974e9aeb501168ce7fcad99be7d96883a8dad
Maikindustries <a01750145@itesm.mx>
2022-11-11 20:20:34 -0600

    Revert "q"
    This reverts commit b39380087b19bb7124fc15af72c3cc05e10e12be.

--- tests/test_part1.py
+++ tests/test_part1.py
@@ -2,11 +2,11 @@ import unittest
 import sys
 import os
  
-# current = os.path.dirname(os.path.realpath(__file__))
-# parent_directory = os.path.dirname(current)
-# sys.path.append(parent_directory)
+current = os.path.dirname(os.path.realpath(__file__))
+parent_directory = os.path.dirname(current)
+sys.path.append(parent_directory)
 
-from ..src.Part1 import SentimentAnalysis
+from src.Part1 import SentimentAnalysis
 
 class test_part1(unittest.TestCase):
   def test_1(self):
